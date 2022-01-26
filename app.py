cfab6712fc073db3e495bc3c2c8b3f79fafbcf46
pixelready <bizartist@gmail.com>
2022-01-26 14:09:12 -0800

    added add and sub routes

--- app.py
+++ app.py
@@ -1,1 +1,23 @@
+from flask import Flask, request
+from operations import add, sub, mult, div
+
 # Put your app in here.
+
+app = Flask(__name__)
+
+@app.get('/add/')
+def add_request():
+    """return sum of a and b"""
+
+    a = request.args.get("a", type=int)
+    b = request.args.get("b", type=int)
+
+    return f"a + b = {add(a,b)}"
+
+@app.get('/sub/')
+def sub_request():
+    """return difference of a minus b"""
+
+    a = request.args.get('a')
+    b = request.args.get('b')
+    return f"a - b = {sub(a,b)}"
